7d90dd09109aef1c40f5e9cd5eefe106405d0393
Katerina Ekaterina <mr_kiskers@mail.ru>
2023-07-20 18:52:37 +0300

    katy322

--- src/main/java/com/example/dream_job/controller/CandidateController.java
+++ src/main/java/com/example/dream_job/controller/CandidateController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/candidates")
+@RequestMapping("/candidates")///
 public class CandidateController {
 
     private final CandidateService candidateService;
